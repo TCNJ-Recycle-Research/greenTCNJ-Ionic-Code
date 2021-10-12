b4dde34cfa899aa188734f2d9fe30c0ff2e7d4e2
pepped1 <pepped467@yahoo.com>
2021-10-12 19:27:10 -0400

    name and package updates

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   "appId": "io.ionic.starter",
-  "appName": "ResearchRecycle",
+  "appName": "GreenTCNJ",
   "bundledWebRuntime": false,
   "webDir": "www",
   "plugins": {
