1dfbdb4b5ad29e340b3f31e0bd5304317296bb13
pepped1 <pepped467@yahoo.com>
2021-10-12 19:57:31 -0400

    id and version number changes

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 
 const config: CapacitorConfig = {
-  "appId": "io.ionic.starter",
+  "appId": "io.ionic.greentcnj",
   "appName": "GreenTCNJ",
   "bundledWebRuntime": false,
   "webDir": "www",
