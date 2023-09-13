964b5e664f337172f8f1ec3449ce4695175d9a13
Elijah Gasataya <gasatae1@tcnj.edu>
2023-09-12 22:27:25 -0400

    BUG: app-version not showing in About me page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,16 +4,20 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+import { AppVersion } from "@ionic-native/app-version/ngx";
+
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+  selector: "app-root",
+  templateUrl: "app.component.html",
+  styleUrls: ["app.component.scss"],
 })
 export class AppComponent {
+  protected app_version: string;
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private appVersion: AppVersion
   ) {
     this.initializeApp();
   }
@@ -21,6 +25,14 @@ export class AppComponent {
   initializeApp() {
     this.platform.ready().then(() => {
       this.statusBar.styleBlackOpaque();
+      // Added 9/12/23 to utilize app_version variable from app-version npm package
+      this.appVersion.getVersionNumber().then(
+        (versionNumber) => {
+          this.app_version = versionNumber;
+        },
+        (error) => {
+          console.log(error);
+        });
       this.splashScreen.show();
     });
   }
