6d1599adad4ff04f4412be274903d45d63e48594
Elijah Gasataya <gasatae1@tcnj.edu>
2023-11-30 17:09:24 -0500

    Added functionality for account deletion

--- src/app/pages/settings/settings.page.ts
+++ src/app/pages/settings/settings.page.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, Renderer2 } from '@angular/core';
-
 import { AuthenticationService } from '../../services/authentication.service';
 import { Router, NavigationExtras  } from '@angular/router';
 import { HttpClient, HttpHeaders} from '@angular/common/http';
+import { AlertController, LoadingController } from '@ionic/angular';
 
 import { Storage } from '@ionic/storage';
 
@@ -29,7 +29,7 @@ export class SettingsPage {
 
   buttonDisabled: any;
 
-  constructor(private authService: AuthenticationService, private router: Router, private renderer: Renderer2, private storage: Storage, public http: HttpClient) { 
+  constructor(private authService: AuthenticationService, private router: Router, private renderer: Renderer2, private storage: Storage, public http: HttpClient, private alertController: AlertController) { 
 
     storage.get('userID').then((val) => {
       this.userID = val;
@@ -149,6 +149,50 @@ export class SettingsPage {
 
   }
 
+  // Asynchronus function will delete the current userID stored locally in device, which will be sent to live server to remove same userID entry
+  async deleteAccount(){
+
+    var confirm_delete = false;
+
+    // Custom modal screen for account deletion confirmation
+    const confirm_alert = await this.alertController.create({
+      header: 'Delete Confirmation',
+      message: 'Are you sure you want to delete your account? You will be redirected back to the login page.',
+      buttons: [
+          {
+            // Do nothing if the user clicks cancel
+            text: 'Cancel',
+          },
+          {
+            text: 'Delete',
+            handler: () => {
+
+              // We will call the same PHP function in users-table.php in the live server for account deletion from the admin portal
+              var arr_id = [];
+              arr_id.push(this.userID);
+              var obj = {func: "delete_users", userIDs: arr_id};
+
+              this.http.post("https://recycle.hpc.tcnj.edu/php/users-handler.php", JSON.stringify(obj)).subscribe(data => {
+
+                var result = data as any[];
+
+                if(result['missingInput']){
+                    console.log("Account was not deleted!");
+                } else {
+                    console.log("Account sucessfully deleted");
+                    // Make sure to logout of the app after the user deletes their account
+                    this.logout();
+                }
+
+              });
+            }
+          }
+      ]
+    });
+    await confirm_alert.present();
+    
+  }
+
 
   async logout() {
     // clears local storage 
@@ -157,6 +201,7 @@ export class SettingsPage {
     this.router.navigateByUrl('/', { replaceUrl: true });
   }
 
+  // This function will be called when the user toggles the themeing functionality for dark/light theme
   onClick(event){
     if(event.detail.checked){
       this.renderer.setAttribute(document.body, 'color-theme', 'dark');
